3ae8cd70fa2773285fe46f75997930d96f399d08
e1ay <rrtervix@yandex.ru>
2022-02-15 16:38:34 +0500

    start

---
+++ main.py
@@ -0,0 +1,56 @@
+from flask import Flask, flash, request, redirect, url_for, render_template
+import urllib.request
+import os
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = 'static/img/'
+
+
+app.secret_key = "secret key"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/')
+def home():
+    ffile = os.listdir(UPLOAD_FOLDER)
+    return render_template('index.html', folder=UPLOAD_FOLDER, ffile=ffile)
+
+
+@app.route('/', methods=['POST'])
+def upload_image():
+    ffile = os.listdir(UPLOAD_FOLDER)
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        flash('Image successfully uploaded and displayed below')
+        redirect(request.url)
+        return render_template('index.html', filename=filename, folder=UPLOAD_FOLDER, ffile=ffile)
+
+    else:
+        flash('Allowed image types are - png, jpg, jpeg, gif')
+        return redirect(request.url)
+
+
+@app.route('/display/<filename>')
+def display_image(filename):
+    return redirect(url_for('static', filename='img/' + filename))
+
+
+if __name__ == "__main__":
+    app.run()
